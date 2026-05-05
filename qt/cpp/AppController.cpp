6bdde855716dd55ee5f54fd71e4a4b10f3c6eaee
SEDERProductions <info@sederproductions.com>
2026-05-05 11:44:37 +0300

    Adjust offload completion status and log messaging (#3)

--- qt/cpp/AppController.cpp
+++ qt/cpp/AppController.cpp
@@ -148,8 +148,14 @@ void AppController::startOffload()
     connect(worker, &DitOffloadWorker::finished, this, [this](const FinalReportData &report) {
         setBusy(false);
         setOverallProgress(1.0);
-        setStatusText(QStringLiteral("Offload complete."));
         setPass(report.allPass);
+        if (report.allPass) {
+            setStatusText(QStringLiteral("Offload complete."));
+            appendLog(QStringLiteral("Offload complete."));
+        } else {
+            setStatusText(QStringLiteral("Offload completed with errors."));
+            appendLog(QStringLiteral("Offload completed with errors."));
+        }
         m_totalFiles = report.totalFiles;
         m_totalSize = report.totalSize;
         m_txtExport = report.txtExport;
@@ -160,7 +166,6 @@ void AppController::startOffload()
         emit exportStateChanged();
         emit canExportMhlChanged();
         emit summaryChanged();
-        appendLog(QStringLiteral("Offload complete."));
     });
     connect(worker, &DitOffloadWorker::failed, this, [this](const QString &message) {
         setBusy(false);
